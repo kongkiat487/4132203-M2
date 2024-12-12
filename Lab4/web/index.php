87d9a693aec52cfccfe89b5269ccf409229cf94a
kong487 <kongkiat070147@gmail.com>
2024-12-12 15:47:36 +0700

    lab4

---
+++ Lab4/web/index.php
@@ -0,0 +1,42 @@
+<?php
+
+echo "Hello !!!";
+
+$name = "kongkiat";
+$age = 20;
+
+$arr = array("name", 20);
+$assoc_arr = ["name"=>"kong", "age"=>20];
+
+echo $arr[0];
+echo $assoc_arr["age"];
+echo $arr;
+echo "<br>";
+var_dump($assoc_arr);
+echo "<br>";
+echo "Hello $name";
+echo 'Hello $name';  //**** */
+
+
+$object = (object)$assoc_arr;
+echo $object->name;
+
+function plus($a, $b)
+{
+    return $a + $b;
+}
+
+$add_name = function($name){
+    echo "Hello $name";
+};
+
+$car = (object)["color"=> "red", "name"=> $add_name];
+// echo $car->name("test");
+
+?>
+
+<?php
+phpinfo()
+
+
+?>
